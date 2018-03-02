bfd5b13d404e863bf355e98f42bb2595896896e0
Kaitlan Rawson <kaitlan.rawson@gmail.com>
2018-03-01 18:32:54 -0700

    Nav bar looks better along with other stuff

--- src/components/Home/Title/Title.js
+++ src/components/Home/Title/Title.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
+import { Parallax } from 'react-parallax'
 
 class Title extends Component {
 
     render(){
         return(
-             <section className = 'above-fold'>
+             <Parallax bgImage = 'http://hddesktopwallpapers.in/wp-content/uploads/2015/09/hd-waterfall-wallpaper.jpg' strength = {700} className = 'above-fold'>
                     <div className = 'title'> 
                         Charitable Animals
                     </div>
-                
-            </section>
+            </Parallax>
 
         )
     }
